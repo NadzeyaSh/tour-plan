19bd73fb7c19399932af5617a9e93c786744a580
NadzeyaSh <shutko_bstu@mail.ru>
2022-01-24 19:21:31 +0300

    fix: fix style for hotel-slider

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
 });
+// const sliderReviews = new Swiper(".reviews-slider", {
+//   // Optional parameters
+//   loop: true,
 
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".slider-button--next",
+//     prevEl: ".slider-button--prev",
+//   },
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: false,
+//   },
+// });
 
-    // Функция ymaps.ready() будет вызвана, когда
-    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-    ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [7.890867336450892, 98.29475612623534],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 15
-        });
-    }
-
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [7.890867336450892, 98.29475612623534],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 15,
+  });
+}
