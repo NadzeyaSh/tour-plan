9a4d633c7263bbe26022a9012d691856620ec062
NadzeyaSh <shutko_bstu@mail.ru>
2022-02-01 10:13:34 +0300

    create: open/close Modal

--- js/main.js
+++ js/main.js
@@ -1,70 +1,89 @@
-const hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-const sliderReviews = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  autoHeight: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
-function init() {
-  // Создание карты.
-  var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [7.890867336450892, 98.29475612623534],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 15,
-  });
-  (myPlacemark1 = new ymaps.Placemark(
-    [7.890867336450892, 98.29475612623534],
-    {
-      // Свойства.
-      // Содержимое иконки, балуна и хинта.
-      iconContent: "Grand Hilton Hotel",
-      balloonContent: "Балун",
-      hintContent: "Стандартный значок метки",
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
     },
-    {
-      // Опции.
-      // Стандартная фиолетовая иконка.
-      preset: "twirl#violetIcon",
-    }
-  )),
-    myMap.geoObjects.add(myPlacemark1);
-}
-$(".parallax-window").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  const sliderReviews = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    autoHeight: true,
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  ymaps.ready(init);
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [7.890867336450892, 98.29475612623534],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 15,
+    });
+    (myPlacemark1 = new ymaps.Placemark(
+      [7.890867336450892, 98.29475612623534],
+      {
+        // Свойства.
+        // Содержимое иконки, балуна и хинта.
+        iconContent: "Grand Hilton Hotel",
+        balloonContent: "Балун",
+        hintContent: "Стандартный значок метки",
+      },
+      {
+        // Опции.
+        // Стандартная фиолетовая иконка.
+        preset: "twirl#violetIcon",
+      }
+    )),
+      myMap.geoObjects.add(myPlacemark1);
+  }
+  $(".parallax-window").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
+
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $('[data-toggle="modal"]');
+  modalButton.on("click", openModal);
+  var closeButton = $(".modal__close");
+  closeButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
