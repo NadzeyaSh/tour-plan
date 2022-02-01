4b60e44ab3e06bbcd98185a952e7b97905df3e4a
NadzeyaSh <shutko_bstu@mail.ru>
2022-02-01 12:22:24 +0300

    create: form validation

--- js/main.js
+++ js/main.js
@@ -93,4 +93,20 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "At least 2 symbols required",
+        },
+        phone: "Please enter your phone number",
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+      },
+    });
+  });
 });
