3cfe4ce6bccbd4c935cbb7acac3b9eb05ac157b1
NadzeyaSh <shutko_bstu@mail.ru>
2022-02-02 12:57:48 +0300

    fix: overlay for modal form

--- js/main.js
+++ js/main.js
@@ -83,6 +83,7 @@ $(document).ready(function () {
   function openModal() {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
+    $("body").css("overflow", "hidden");
     modalOverlay.addClass("modal__overlay--visible");
     modalDialog.addClass("modal__dialog--visible");
   }
@@ -90,6 +91,7 @@ $(document).ready(function () {
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
+    $("body").css("overflow", "display");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
